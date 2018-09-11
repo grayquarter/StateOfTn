6562da8efed53044ceff187a738ec9dd77e4236c
John Schomp <john@grayquarter.com>
2018-09-11 12:31:26 -0700

    Updates

--- Scripts/Event/WTUA;TNABC!PERMITS!APPLICATION!SERVER.js
+++ Scripts/Event/WTUA;TNABC!PERMITS!APPLICATION!SERVER.js
@@ -1,70 +1,66 @@
-try{
-	
-	var capIDScriptModel = aa.cap.createCapIDScriptModel(capId.getID1(),capId.getID2(),capId.getID3());
+try {
+
+	var capIDScriptModel = aa.cap.createCapIDScriptModel(capId.getID1(), capId.getID2(), capId.getID3());
 	var examPassed = false;
-	
+
 	//business process: create record; enter exam; process workflow........
-	if (wfTask.equals("Preliminary Review") && wfStatus.equals("Approved")){
+	if (wfTask.equals("Preliminary Review") && wfStatus.equals("Approved")) {
 		var didExamPass = getCapExamStatus(examPassed);
-		if (didExamPass){
-			var asiLicenseCounty = AInfo["County"]; 
-			
+		if (didExamPass) {
+			var asiLicenseCounty = AInfo["County"];
+
 			comment("Permit COUNTY =  " + asiLicenseCounty);
-			
-			if (asiLicenseCounty != null){
-				var spExpireDate = dateAddMonths(null, 60);			
-				var contactType = TNABCgetContactType("TNABC","Permits","NA","Server");
-				spCapId=createPermit("TNABC","Permits","NA","Server","","Active",true,false,contactType,false,"",asiLicenseCounty);
+
+			if (asiLicenseCounty != null) {
+				var spExpireDate = dateAddMonths(null, 60);
+				var contactType = TNABCgetContactType("TNABC", "Permits", "NA", "Server");
+				spCapId = createPermit("TNABC", "Permits", "NA", "Server", "", "Active", true, false, contactType, false, "", asiLicenseCounty);
 
 				logDebug("Server Permit Expiration Date: " + spExpireDate);
-				
-				
+
 				holdId = capId;
 				capId = spCapId;
-				
-				copyASIFields(holdId,capId);
+
+				copyASIFields(holdId, capId);
 				editAppName("Created from Application #" + capIDString);
-				updateTask("Permit Status", "Active","Updated via EMSE Script","");				
+				updateTask("Permit Status", "Active", "Updated via EMSE Script", "");
 				licEditExpInfo("Active", spExpireDate);
-				
-				capId = holdId;				
-				
+
+				capId = holdId;
+
 				spTemplateName = "TABC_LICENSE_APPROVAL";
-				sendNotificationToContactTypes("Permittee",spTemplateName);
-				
-				closeTask("Application Status","Issued","Updated via Exam Upload Script","");
+				sendNotificationToContactTypes("Permittee", spTemplateName);
+
+				closeTask("Application Status", "Issued", "Updated via Exam Upload Script", "");
 
 			}
 		}
 	}
+} catch (err) {
+	aa.print("A JavaScript Error occured: " + err.message + " at line " + err.lineNumber + " stack: " + err.stack);
 }
-catch (err) {
-            aa.print("A JavaScript Error occured: " + err.message + " at line " + err.lineNumber + " stack: "+ err.stack);
-            }
-
 
 aa.print("Final Result: " + didExamPass);
 
-function getCapExamStatus(examPassed){
+function getCapExamStatus(examPassed) {
 	var examListResult = aa.examination.getExaminationList(capIDScriptModel);
 
-	if (examListResult.getSuccess){
+	if (examListResult.getSuccess) {
 		examList = examListResult.getOutput();
 		aa.print(examList.length);
-		
-		for (eL in examList){
+
+		for (eL in examList) {
 			//objectExplore(examList[eL]);
 			thisExam = examList[eL].getExaminationModel();
 			examStatus = thisExam.getFinalScore();
 			examPassing = thisExam.getPassingScore();
-			aa.print("Status: "+ examStatus);
+			aa.print("Status: " + examStatus);
 			aa.print("Passing: " + examPassing);
-			
-			if (examStatus != null && Number(examStatus) >= Number(examPassing)){
+
+			if (examStatus != null && Number(examStatus) >= Number(examPassing)) {
 				examPassed = true;
 				aa.print("Pass it");
-			}
-			else
+			} else
 				aa.print("Don't");
 		}
 	}
--- Scripts/Event/WTUB;TNABC!PERMITS!APPICATION!SERVER.js
+++ Scripts/Event/WTUB;TNABC!PERMITS!APPICATION!SERVER.js
@@ -1,49 +1,45 @@
 // TODO is this file name correct?
 
-try{
-	
-	var capIDScriptModel = aa.cap.createCapIDScriptModel(capId.getID1(),capId.getID2(),capId.getID3());
+try {
+
+	var capIDScriptModel = aa.cap.createCapIDScriptModel(capId.getID1(), capId.getID2(), capId.getID3());
 	var examPassed = false;
-	
+
 	//business process: create record; enter exam; process workflow........
-	if (wfTask.equals("Preliminary Review") && wfStatus.equals("Approved")){
-		var outStateExam = AInfo["Class Out of State"]; 
+	if (wfTask.equals("Preliminary Review") && wfStatus.equals("Approved")) {
+		var outStateExam = AInfo["Class Out of State"];
 		var didExamPass = getCapExamStatus(examPassed);
-		if (!didExamPass && outStateExam == "Yes"){
-			showMessage =true;
+		if (!didExamPass && outStateExam == "Yes") {
+			showMessage = true;
 			comment("This review cannot be approved;  A passing exam does not exist.  Please complete the exam information before Approving the Preliminary Review");
-cancel = true;
+			cancel = true;
 		}
 	}
+} catch (err) {
+	aa.print("A JavaScript Error occured: " + err.message + " at line " + err.lineNumber + " stack: " + err.stack);
 }
-
-catch (err) {
-            aa.print("A JavaScript Error occured: " + err.message + " at line " + err.lineNumber + " stack: "+ err.stack);
-            }
-
 
 aa.print("Final Result: " + didExamPass);
 
-function getCapExamStatus(examPassed){
+function getCapExamStatus(examPassed) {
 	var examListResult = aa.examination.getExaminationList(capIDScriptModel);
 
-	if (examListResult.getSuccess){
+	if (examListResult.getSuccess) {
 		examList = examListResult.getOutput();
 		aa.print(examList.length);
-		
-		for (eL in examList){
+
+		for (eL in examList) {
 			//objectExplore(examList[eL]);
 			thisExam = examList[eL].getExaminationModel();
 			examStatus = thisExam.getFinalScore();
 			examPassing = thisExam.getPassingScore();
-			aa.print("Status: "+ examStatus);
+			aa.print("Status: " + examStatus);
 			aa.print("Passing: " + examPassing);
-			
-			if (examStatus != null && Number(examStatus) >= Number(examPassing)){
+
+			if (examStatus != null && Number(examStatus) >= Number(examPassing)) {
 				examPassed = true;
 				aa.print("Pass it");
-			}
-			else
+			} else
 				aa.print("Don't");
 		}
 	}
