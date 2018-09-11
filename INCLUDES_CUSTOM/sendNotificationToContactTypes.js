55da4504aa54fb29a0f10fd32f6aff12f6be4a13
John Schomp <john@grayquarter.com>
2018-09-11 12:55:13 -0700

    truncated functions

--- INCLUDES_CUSTOM/sendNotificationToContactTypes.js
+++ INCLUDES_CUSTOM/sendNotificationToContactTypes.js
@@ -8,3 +8,77 @@ function sendNotificationToContactTypes(sendEmailToContactTypes, emailTemplate) 
 	var altId = thisCapId.getCustomID();
 
 	if (arguments.length < 2) {
+		logDebug("ERROR: Function sendNotificationToContactTypes requres two arguments.  Usage: sendNotificationToContactTypes(string sendEmailToContactTypes, string emailTemplate)");
+		return false
+	}
+
+	if (sendEmailToContactTypes == null || sendEmailToContactTypes.length == 0) {
+		logDebug("ERROR: No contact types list found.");
+		return false;
+	}
+
+	if (emailTemplate == null || emailTemplate.length == 0) {
+		logDebug("ERROR: No email template submitted.");
+		return false;
+	}
+
+	if (sendEmailToContactTypes.length > 0 && emailTemplate.length > 0) {
+
+		var conTypeArray = sendEmailToContactTypes.split(",");
+		var conArray = getContactArray(thisCapId);
+
+		if (conArray && conArray.length > 0)
+			logDebug("Have the contactArray");
+
+		for (thisCon in conArray) {
+			conEmail = null;
+			b3Contact = conArray[thisCon];
+			if (exists(b3Contact["contactType"], conTypeArray)) {
+				conEmail = b3Contact["email"];
+				logDebug(altId + ": Contact type " + b3Contact["contactType"] + " found. Email: " + conEmail);
+			}
+
+			if (conEmail) {
+				emailParameters = aa.util.newHashtable();
+				getContactParams4Notification(emailParameters, b3Contact["contactType"]);
+				getRecordParams4Notification(emailParameters);
+				
+				var acaURL = "https://rlpsdev.abc.tn.gov/devtabc";
+				getRecordParams4Notification_mod(emailParameters);
+				getACARecordParam4Notification(emailParameters,acaURL);
+				
+				/* addParameter(emailParameters, "$$businessName$$", cap.getSpecialText()); */
+				addParameter(emailParameters, "$$altID$$", capIDString);	
+				addParameter(emailParameters, "$$recordType$$", recordType);
+				addParameter(emailParameters, "$$acaUrl$$", acaURL+getACAUrl());
+				addParameter(emailParameters, "$$acaUrlOnly$$", acaURL);
+				
+				//if (wfComment)
+					addParameter(emailParameters, "$$wfComment$$", "");
+				//if (wfStatus)
+					addParameter(emailParameters, "$$wfStatus$$", "");
+                              
+                try {
+					b1ExpResult = aa.expiration.getLicensesByCapID(capId);
+					if (b1ExpResult.getSuccess()) {
+						this.b1Exp = b1ExpResult.getOutput();
+						var tmpDate = this.b1Exp.getExpDate();
+						var expDate = tmpDate.getMonth() + "/" + tmpDate.getDayOfMonth() + "/" + tmpDate.getYear();
+						addParameter(emailParameters, "$$expirationDate$$", expDate);
+					}
+				} catch (err) {
+					logDebug("Expiration Date does not apply for permit # " + altId);
+				}
+				                       
+				var capId4Email = aa.cap.createCapIDScriptModel(thisCapId.getID1(), thisCapId.getID2(), thisCapId.getID3());
+
+				var fileNames = [];
+				
+				logDebug("ACA URL: " + acaURL);
+
+				aa.document.sendEmailAndSaveAsDocument(mailFrom, conEmail, "", emailTemplate, emailParameters, capId4Email, fileNames);
+				logDebug(altId + ": Sent Email template " + emailTemplate + " to " + b3Contact["contactType"] + " : " + conEmail);
+			}
+		}
+	}
+}
--- INCLUDES_CUSTOM/sendNotificationToContactTypesTN.js
+++ INCLUDES_CUSTOM/sendNotificationToContactTypesTN.js
@@ -7,3 +7,65 @@ function sendNotificationToContactTypesTN(sendEmailToContactTypes, emailTemplate
 	var altId = thisCapId.getCustomID();
 
 	if (arguments.length < 2) {
+		logDebug("ERROR: Function sendNotificationToContactTypes requres two arguments.  Usage: sendNotificationToContactTypes(string sendEmailToContactTypes, string emailTemplate)");
+		return false
+	}
+
+	if (sendEmailToContactTypes == null || sendEmailToContactTypes.length == 0) {
+		logDebug("ERROR: No contact types list found.");
+		return false;
+	}
+
+	if (emailTemplate == null || emailTemplate.length == 0) {
+		logDebug("ERROR: No email template submitted.");
+		return false;
+	}
+
+	if (sendEmailToContactTypes.length > 0 && emailTemplate.length > 0) {
+
+		var conTypeArray = sendEmailToContactTypes.split(",");
+		var conArray = getContactArray(thisCapId);
+
+		if (conArray && conArray.length > 0)
+			logDebug("Have the contactArray");
+
+		for (thisCon in conArray) {
+			conEmail = null;
+			b3Contact = conArray[thisCon];
+			if (exists(b3Contact["contactType"], conTypeArray)) {
+				conEmail = b3Contact["email"];
+				logDebug(altId + ": Contact type " + b3Contact["contactType"] + " found. Email: " + conEmail);
+			}
+
+			if (conEmail) {
+				emailParameters = aa.util.newHashtable();
+				getContactParams4Notification(emailParameters, b3Contact["contactType"]);
+				getRecordParams4Notification(emailParameters);
+				/* addParameter(emailParameters, "$$businessName$$", cap.getSpecialText()); */
+                addParameter(emailParameters, "$$wfComment$$", wfComment);
+				addParameter(emailParameters, "$$capType$$", capType);
+				addParameter(emailParameters, "$$altID$$", capIDString);
+                addParameter(emailParameters, "$$wfStatus$$", wfStatus);
+                addParameter(emailParameters, "$$TNComment$$", TNcomment);                
+                                   try {
+					b1ExpResult = aa.expiration.getLicensesByCapID(capId);
+					if (b1ExpResult.getSuccess()) {
+						this.b1Exp = b1ExpResult.getOutput();
+						var tmpDate = this.b1Exp.getExpDate();
+						var expDate = tmpDate.getMonth() + "/" + tmpDate.getDayOfMonth() + "/" + tmpDate.getYear();
+						addParameter(emailParameters, "$$expirationDate$$", expDate);
+					}
+				} catch (err) {
+					logDebug("Expiration Date does not apply for permit # " + altId);
+				}
+				                       
+				var capId4Email = aa.cap.createCapIDScriptModel(thisCapId.getID1(), thisCapId.getID2(), thisCapId.getID3());
+
+				var fileNames = [];
+
+				aa.document.sendEmailAndSaveAsDocument(mailFrom, conEmail, "", emailTemplate, emailParameters, capId4Email, fileNames);
+				logDebug(altId + ": Sent Email template " + emailTemplate + " to " + b3Contact["contactType"] + " : " + conEmail);
+			}
+		}
+	}
+}
